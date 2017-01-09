3981b84ffdcaa044f9e1f466e64283e797524597
Михаил <muuwka@gmail.com>
2017-01-09 13:48:33 +0200

    api

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,14 @@
-$( document ).ready(function() {
-    $.ajax( "/register/langlist" )
-        .done(function(data) {
-            $.each(JSON.parse(data), function(k,v) {
-                $('#lang_select').append('<option value="'+v.alpha2+'">'+v.name+'</option>');
-            });
-        });
-    $.ajax( "/register/countrylist" )
-        .done(function(data) {
-            $.each(JSON.parse(data), function(k,v) {
-                $('#country_select').append('<option value="'+v.code+'">'+v.name+'</option>');
-            });
-        });
-});
-   
+// $( document ).ready(function() {
+//     $.ajax( "/register/langlist" )
+//         .done(function(data) {
+//             $.each(JSON.parse(data), function(k,v) {
+//                 $('#lang_select').append('<option value="'+v.alpha2+'">'+v.name+'</option>');
+//             });
+//         });
+//     $.ajax( "/register/countrylist" )
+//         .done(function(data) {
+//             $.each(JSON.parse(data), function(k,v) {
+//                 $('#country_select').append('<option value="'+v.code+'">'+v.name+'</option>');
+//             });
+//         });
+// });
