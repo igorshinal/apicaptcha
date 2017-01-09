0f5f031a0855af675e879262b4fbde1e97f5edda
Михаил <muuwka@gmail.com>
2017-01-09 18:09:08 +0200

    api

--- resources/views/profile/profile-info.blade.php
+++ resources/views/profile/profile-info.blade.php
@@ -9,7 +9,6 @@
             <div class="col-xs-12 col-sm-8">
                 <ul class="list-group">
                     <li class="list-group-item"><i class="glyphicon glyphicon-user"></i>  {{ Auth::user()->company_name }} ({{ Auth::user()->name }})</li>
-                    <li class="list-group-item"><i class="glyphicon glyphicon-cloud"></i> {{ $country }} ({{ $lang }})</li>
                     @if (Auth::user()->phone)<li class="list-group-item"><i class="fa fa-phone"></i>  {{ Auth::user()->phone }} </li> @endif
                     <li class="list-group-item"><i class="fa fa-envelope"></i>  {{ Auth::user()->email }}</li>
                     <li class="list-group-item"><i class="glyphicon glyphicon-globe"></i>  {{ Auth::user()->website }}</li>
